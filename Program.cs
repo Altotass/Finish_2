d441f07c4befe15b4442534eb6bbd5e13e73e6c7
altotass <altotass@gmail.com>
2022-09-29 12:38:31 +0300

    total_work

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿void PrintArray(string[] array)
-{   for (int i = 0; i < array.Length; i++)
-    {   Console.Write(array[i] + ", ");
-    }
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+void ThreeSymbol()
+{
+void PrintArray(string[] array)
+{   array = array.Where(x => x != null).ToArray();   
+        for (int i = 0; i < array.Length; i++)
+        {   Console.Write(array[i] + ", ");
+        }
     Console.WriteLine();
 }
 Console.WriteLine("Сколько элементов ввести с клавиатуры?");
@@ -15,11 +27,12 @@ string[] massiv1 = new string[x];
 string[] massiv2 = new string[x];
 int k = 0;
     for (int j = 0; j < x; j++)
-    {
-        if (massiv1[j].Length <= 3)
+    {   if (massiv1[j].Length <= 3)
         {   massiv2[k] = massiv1[j];
             k++;
         }
     }
 Console.WriteLine();
 PrintArray(massiv2);
+}
+ThreeSymbol();
